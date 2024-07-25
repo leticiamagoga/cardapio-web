13b1ebf702641d6e73c9eaf86ff4a7a618d95812
leticiamagoga <leticiamagogasfs@gmail.com>
2024-07-25 10:44:07 -0300

    criado repositorio de categoria

---
+++ Repositories/CategoriaRepository.cs
@@ -0,0 +1,20 @@
+﻿using CardapioWeb.Models;
+using Microsoft.EntityFrameworkCore;
+using CardapioWeb.Context;
+namespace CardapioWeb.Repositories
+{
+    public class CategoriaRepository : ICategoriaRepository
+    { 
+
+        private readonly AppDBContext _dbcontext;
+        public CategoriaRepository ( AppDBContext dbcontext)
+        {
+        _dbcontext = dbcontext;
+
+        }
+    public IEnumerable<Categoria> GetAll()
+    {
+    return _dbcontext.Categorias.ToList();
+    }
+    }
+}
---
+++ Repositories/ProdutoRepository.cs
@@ -0,0 +1,24 @@
+﻿using CardapioWeb.Context;
+using CardapioWeb.Models;
+
+namespace CardapioWeb.Repositories
+{
+    public class ProdutoRepository : IProdutoRepository
+    {
+        private readonly AppDBContext _dbContext;
+        public IEnumerable<Produto> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Produto GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Produto> GetByPreferido()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
