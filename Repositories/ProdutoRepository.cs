0590756842999e3b2c51108586452bc8972dcd7a
leticiamagoga <leticiamagogasfs@gmail.com>
2024-08-02 10:33:26 -0300

    criado controller e view

--- Repositories/ProdutoRepository.cs
+++ Repositories/ProdutoRepository.cs
@@ -6,19 +6,25 @@ namespace CardapioWeb.Repositories
     public class ProdutoRepository : IProdutoRepository
     {
         private readonly AppDBContext _dbContext;
+
+        public ProdutoRepository(AppDBContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public IEnumerable<Produto> GetAll()
         {
-            throw new NotImplementedException();
+            return _dbContext.Produtos.ToList(); // Busca tudo que há na tabela produtos, ToList converte em uma lista
         }
 
         public Produto GetById(int id)
         {
-            throw new NotImplementedException();
+            return _dbContext.Produtos.FirstOrDefault(p => p.Id == id); // Busca pela referência Id, o FirstOrDefault pega o primeiro encontrado, se não achar envia uma mensagem de erro
         }
 
         public IEnumerable<Produto> GetByPreferido()
         {
-            throw new NotImplementedException();
+            return _dbContext.Produtos.Where(p => p.Produto_Preferido == true).ToList(); // Busca pela referência dos produtos preferidos e monta uma lista
         }
     }
 }
