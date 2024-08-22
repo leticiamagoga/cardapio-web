992e071b5db5d65572aeb55c1c4aac78c5d6acc1
leticiamagoga <leticiamagogasfs@gmail.com>
2024-08-22 10:50:05 -0300

    criançao do crud

--- Repositories/CategoriaRepository.cs
+++ Repositories/CategoriaRepository.cs
@@ -1,20 +1,67 @@
 ﻿using CardapioWeb.Models;
 using Microsoft.EntityFrameworkCore;
 using CardapioWeb.Context;
+
 namespace CardapioWeb.Repositories
 {
     public class CategoriaRepository : ICategoriaRepository
-    { 
+    {
 
         private readonly AppDBContext _dbcontext;
-        public CategoriaRepository ( AppDBContext dbcontext)
+        public CategoriaRepository(AppDBContext dbcontext)
         {
-        _dbcontext = dbcontext;
+            _dbcontext = dbcontext;
 
         }
-    public IEnumerable<Categoria> GetAll()
-    {
-    return _dbcontext.Categorias.ToList();
-    }
+
+        public async Task Add(Categoria categoria)
+        {
+            try
+            {
+                _dbcontext.Categorias.Add(categoria); //ad coisa no bd
+                await _dbcontext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                await Task.FromException(ex);
+            }
+
+        }
+
+        public async Task Delete(Categoria categoria)
+        {
+            try
+            {
+                _dbcontext.Categorias.Remove(categoria); //ad coisa no bd
+                await _dbcontext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                await Task.FromException(ex);
+            }
+        }
+
+        public async Task<Categoria> GetById(int id)
+        {
+            return await _dbcontext.Categorias.FirstOrDefaultAsync(c => c.Id == id);
+        }
+
+        public async Task Update(Categoria categoria)
+        {
+            try
+            {
+                _dbcontext.Categorias.Update(categoria); //ad coisa no bd
+                await _dbcontext.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                await Task.FromException(ex);
+            }
+        }
+
+        public async Task<List<Categoria>> GetAll()
+        {
+            return await _dbcontext.Categorias.ToListAsync();
+        }
     }
 }
